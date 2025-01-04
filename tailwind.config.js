905bdf85b1692411d25febcc4a1499e95f8e6e07
Aya-Ammar <ayaammar2023@gmail>
2025-01-04 10:02:33 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    
+
+    extend: {
+      screens:{
+        '2md':'960px',
+
+      },
+      colors:{
+      primary:'#7E3AF2',
+      secondaryText:'#6B7280',
+      mainText:'#111928'
+               },
+    fontFamily: {
+      Inter:["Inter", "sen-serif"],
+               },
+          },
+  },
+  plugins: [],
+}
+
